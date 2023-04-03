2bb7388fad16b0f23ab6755d4472100e0a207ad1
Muhamad Rafli Susanto <73054214+raflisusanto@users.noreply.github.com>
2023-04-04 00:14:25 +0700

    Fixed cardList styling

--- dummy_data/tips.jsx
+++ dummy_data/tips.jsx
@@ -17,4 +17,28 @@ export const tipsCards = [
     author: "Ini deskripsi tips 3.",
     article: "Test",
   },
+  {
+    id: 4,
+    title: "Tips 4",
+    author: "Ini deskripsi tips 4.",
+    article: "Test",
+  },
+  {
+    id: 5,
+    title: "Tips 5",
+    author: "Ini deskripsi tips 5.",
+    article: "Test",
+  },
+  {
+    id: 6,
+    title: "Tips 6",
+    author: "Ini deskripsi tips 6.",
+    article: "Test",
+  },
+  {
+    id: 7,
+    title: "Tips 7",
+    author: "Ini deskripsi tips 7.",
+    article: "Test",
+  },
 ];
